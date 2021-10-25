c9e7cf0a7de7228aeea25592cc95d97856e34467
lmirenda <86894184+lmirenda@users.noreply.github.com>
2021-10-25 09:21:09 -0300

    Co-authored-by: Felipe Caillabet <fcaillabet@users.noreply.github.com>

---
+++ constructores.js
@@ -0,0 +1,9 @@
+class Usuarios{
+    constructor(_username,_pass,_CI,_nombre,_apellido){
+        this.username = _username;
+        this.pass = _pass;
+        this.cedula = _CI;
+        this.nombre = _nombre;
+        this._apellido = _apellido;
+    }
+}
